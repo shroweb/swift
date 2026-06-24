27cd24fc7efbc5c944a21cd2e352665a43725e0a
Callum MacInnes <shro.app@gmail.com>
2026-06-24 12:34:26 +0100

    FIXES

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -5,4 +5,7 @@ export default defineCliConfig({
     projectId: process.env.SANITY_STUDIO_PROJECT_ID || 'loyzqo4e',
     dataset: process.env.SANITY_STUDIO_DATASET || 'production',
   },
+  deployment: {
+    appId: 'nnqf6i4vysa5ekcoh86b5tn7',
+  },
 });
